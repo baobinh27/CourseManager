d99a96f2b99ed916ddfb6105fbb6d67a21124281
baonguyengia2701 <baonguyengia2701@gmail.com>
2025-03-22 14:09:54 +0700

    feat: login page

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import './App.css';
 import Home from "./frontend/Home.js";
 import MyCourses from "./frontend/MyCourses.js";
 import NotFound from "./frontend/NotFound.js";
+import Login from "./frontend/components/Login";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />}/>
+        <Route path="/login" element={<Login />}/>
         <Route path="/my-courses" element={<MyCourses />}/>
         <Route path="*" element={<NotFound />}/>
       </Routes>
